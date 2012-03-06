20a5f54eb701ab0f26aa5741eb52f551706d3eb3
ecavazos <ejcavazos@gmail.com>
2012-03-05 22:30:36 -0800

    removed a space

--- spec/requests/sign_up_spec.rb
+++ spec/requests/sign_up_spec.rb
@@ -5,9 +5,9 @@ describe 'User Sign Up' do
   it 'allows a user to sign up' do
     visit new_user_path
 
-    fill_in 'Email',                  :with => 'mike.hawk@foo.com'
-    fill_in 'Password',               :with => '1234'
-    fill_in 'Password Confirmation ', :with => '1234'
+    fill_in 'Email',                 :with => 'mike.hawk@foo.com'
+    fill_in 'Password',              :with => '1234'
+    fill_in 'Password Confirmation', :with => '1234'
 
     # submit form and assert user was created successfully
   end
