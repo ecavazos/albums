b16d5b2005a3d8dbecd8ab3a6be1cbc08c08ffd1
ecavazos <ejcavazos@gmail.com>
2012-03-05 21:39:31 -0800

    upgrade to rails 3.2.2

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Albums::Application.config.secret_token = '1b1df099cab87581cd80df5e391ae4a4f934cafc7788f2aef8acb67f23f6a013f1a52f6573de8467f972fd5a73f9bf5c448ef425e0a639700d5316ccff1153a3'
+Albums::Application.config.secret_token = '9b591aa9b084f90a1d3447c60919d4ec681da6d4c24a93e84061841953d95e96256e2b6fa188ae8148492fcc6915fde4378729ff3d8d1dc167e97496d02131f9'
