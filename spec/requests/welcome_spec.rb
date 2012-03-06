593b37f91786a25f134f3b41418136626ea7faf1
ecavazos <ejcavazos@gmail.com>
2012-03-05 22:12:08 -0800

    added some starter specs

--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
 
-describe 'the welcome page' do
-  it 'allows the user to visit' do
+describe 'The Welcome Page' do
+
+  it 'allows an unauthenticated user to visit' do
     visit root_path
+    find('h1').should have_content 'Welcome to Albums'
+  end
+
+  it 'links to the sign up page' do
+    visit root_path
+
+    click_link 'Sign up'
+
+    current_path.should == new_user_path
   end
 end
